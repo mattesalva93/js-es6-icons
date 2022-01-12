4709dd14bad05fb8651871a0c0314ec4ff2dc2bb
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-12 16:08:08 +0100

    not working change

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-[
+const elenco = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +112,84 @@
 		color: 'blue'
 	}
 ];
+
+const elencoAnimali = elenco.filter((elemento) => {
+	if(elemento.type == "animal"){
+		return true
+	} else{
+		return false;
+	} 
+});
+
+const elencoVegetali = elenco.filter((elemento) => {
+	if(elemento.type == "vegetable"){
+		return true
+	} else{
+		return false;
+	} 
+});
+
+const elencoUser = elenco.filter((elemento) => {
+	if(elemento.type == "user"){
+		return true
+	} else{
+		return false;
+	} 
+});
+
+
+let creaBox = document.getElementById("box-container");
+ 
+elenco.forEach(elemento => {
+	generaBox(elemento, creaBox);
+});
+
+let scelta = document.getElementById("type-selection");
+scelta.addEventListener( "change", function() {
+	const sceltaTipo = this.value;
+	console.log(sceltaTipo);
+	;
+	if (sceltaTipo == "all"){
+		elenco.forEach(elemento => {
+			generaBox(elemento, creaBox);
+		});
+	}else if(sceltaTipo == "animal"){
+		elencoAnimali.forEach(elemento => {
+			generaBox(elemento, creaBox);
+		});
+	}else if(sceltaTipo == "vegetable"){
+		elencoVegetali.forEach(elemento => {
+			generaBox(elemento, creaBox);
+		});
+	}else{
+		elencoUser.forEach(elemento => {
+			generaBox(elemento, creaBox);
+		});
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generaBox(elemento,creaBox){
+
+	creaBox.innerHTML += `
+		<div class="col text-center box">
+			<i class="${elemento.family} ${elemento.prefix}${elemento.name} ${elemento.type}" style="color:${elemento.color}"></i>
+			<h6>${elemento.name.toUpperCase()}</h6>
+		</div>  
+	`
+}
